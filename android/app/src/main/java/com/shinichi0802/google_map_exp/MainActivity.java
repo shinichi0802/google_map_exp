80e7cc7564879593dc38d9c935a5a86905b1cc5f
TuanAnh Nguyen <shinichi08022003@yahoo.com>
2019-05-03 10:45:06 +0700

    Initial commit

---
+++ android/app/src/main/java/com/shinichi0802/google_map_exp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.shinichi0802.google_map_exp;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
